2e988bb3b0ec1a46764abbd4308b77c05942e87c
shmuel7e <shmuel7e@gmail.com>
2021-08-13 17:10:47 +0300

    lets see

--- src/cmps/myPortfolio/MyPortfolio.jsx
+++ src/cmps/myPortfolio/MyPortfolio.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import AnimatedLetter from "../animatedLetter";
-import Wrapper from "../../cmps/wrapper";
-import myProfile from "../../assets/images/profile.jpg";
-import olyaBridal from '../../assets/images/olyaBridal.PNG';
-import taskIt from '../../assets/images/taskIt.PNG';
-import mines from '../../assets/images/mines.PNG';
-import soundx from '../../assets/images/soundX.PNG';
-import weather from '../../assets/images/weather.PNG';
+import Wrapper from "../wrapper";
+// import myProfile from "../../assets/images/profile.jpg";
+import olyaBridal from "../../assets/images/olyaBridal.PNG";
+import taskIt from "../../assets/images/taskIt.PNG";
+import mines from "../../assets/images/mines.PNG";
+import soundx from "../../assets/images/soundX.PNG";
+import weather from "../../assets/images/weather.PNG";
+import arduino from "../../assets/images/arduino.PNG";
 
 // const isElementInViewport = (el) => {
 //   var rect = el.getBoundingClientRect();
@@ -45,12 +46,12 @@ const MyPortfolio = () => {
           </div>
         </div>
       </Wrapper>
-      <Wrapper tag={"img"}>
+      {/* <Wrapper tag={"img"}>
         <div className={styles.profile}>
           <img src={myProfile} alt=""></img>
         </div>
-      </Wrapper>
-      <Wrapper tag={"p"}>
+      </Wrapper> */}
+      {/* <Wrapper tag={"p"}>
         <div className={styles.profileTxt}>
           <span>Experienced Full Stack Web Engineer B.Sc. </span>
           <span> adept in all stages of web development.</span>
@@ -62,15 +63,83 @@ const MyPortfolio = () => {
             as collaborate in a team setting.
           </span>
         </div>
+      </Wrapper> */}
+      <Wrapper tag={"p"}>
+        <div className={styles.profileTxt}>
+          A small gallery of recent projects.
+          <span>
+            These projects mainly focus on javaScript frameworks such as
+            React.js.
+          </span>
+          <span>
+            some of the work is freelance and based on single page application.
+          </span>
+          <span className={styles.subTxt}>
+            interested to see some more? Visit{" "}
+            <a href={"https://github.com/shmuel7e"}>
+              <span>my work</span>
+            </a>{" "}
+            page.
+          </span>
+        </div>
       </Wrapper>
       <div className={styles.portfolioContainer}>
         <Wrapper tag={"section"}>
           <div className={styles.portfolio}>
-           <div> <img  onClick={()=>  window.location = "http://www.olyabridal.com" } src={olyaBridal} alt=""></img></div>
-          <div>  <img  onClick={()=>  window.location = "https://app-taskit.herokuapp.com/guest" }src={taskIt} alt=""></img></div>
-           <div> <img  onClick={()=>  window.location = "https://shmuelmusicx.netlify.app/" } src={soundx} alt=""></img></div>
-           <div> <img  onClick={()=>  window.location = "https://shmuelminesweeper.netlify.app/" } src={mines} alt=""></img></div>
-           <div> <img  onClick={()=>  window.location = "https://shmuel7e.github.io/shmuel-elkis-27-11-2020" } src={weather} alt=""></img></div>
+            <div>
+              <img
+                onClick={() => (window.location = "http://www.olyabridal.com")}
+                src={olyaBridal}
+                alt=""
+              ></img>
+            </div>
+            <div>
+              <img
+                onClick={() =>
+                  (window.location = "https://app-taskit.herokuapp.com/guest")
+                }
+                src={taskIt}
+                alt=""
+              ></img>
+            </div>
+            <div>
+              <img
+                onClick={() =>
+                  (window.location = "https://shmuelmusicx.netlify.app/")
+                }
+                src={soundx}
+                alt=""
+              ></img>
+            </div>
+            <div>
+              <img
+                onClick={() =>
+                  (window.location = "https://shmuelminesweeper.netlify.app/")
+                }
+                src={mines}
+                alt=""
+              ></img>
+            </div>
+            <div>
+              <img
+                onClick={() =>
+                  (window.location =
+                    "https://shmuel7e.github.io/shmuel-elkis-27-11-2020")
+                }
+                src={weather}
+                alt=""
+              ></img>
+            </div>
+            <div>
+              <img
+                onClick={() =>
+                  (window.location =
+                    "https://shmuel7e.github.io/shmuel-elkis-27-11-2020")
+                }
+                src={arduino}
+                alt=""
+              ></img>
+            </div>
           </div>
         </Wrapper>
       </div>
@@ -78,4 +147,3 @@ const MyPortfolio = () => {
   );
 };
 export default React.memo(MyPortfolio);
-
