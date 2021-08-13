8944593cef060cec0e85d0cd4796329fd42753cf
shmuel7e <shmuel7e@gmail.com>
2021-08-13 11:31:00 +0300

    alot of changes

--- src/cmps/into/Intro.jsx
+++ src/cmps/into/Intro.jsx
@@ -22,7 +22,7 @@ const Intro = () => {
       <Scroll direction={"left"} />
       <Wrapper tag={"h1"}>
         {
-          <Suspense fallback={<div style={{ minHeight: "157px" }}></div>}>
+          <Suspense fallback={<div style={{ minHeight: "123px" }}></div>}>
             <AnimatedTitle />
           </Suspense>
         }
